10bf3cdd19e5eca8827ac3eec37cdd1507c68c3c
a.gusejnov <gusejnov1999@list.ru>
2018-06-06 20:32:59 +0300

    add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroesComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeroesComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatInputModule,
+        BrowserAnimationsModule
+    ],
+    exports: [MatButtonModule, MatCheckboxModule, MatInputModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  public hero = 'WindStorm';
-  constructor() {
+    public hero: Hero = {
+        id: 1,
+        name: 'WindStorm'
+    };
 
-  }
+    constructor() {
 
-  ngOnInit() {
-  }
+    }
+
+    ngOnInit() {
+    }
 
 }
