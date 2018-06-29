604241e6fbc6b828f4480a57460b0382f4bbd8b1
a.gusejnov <gusejnov1999@list.ru>
2018-06-29 23:54:03 +0300

    notificitaion methods

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../message.service';
+import {Component, OnInit} from '@angular/core';
+import {MessageService} from '../message.service';
+import {MatSnackBar} from '@angular/material';
+// import {MAT_SNACK_BAR_DATA} from '@angular/material';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messagesService: MessageService) { }
+    constructor(
+        public messagesService: MessageService,
+        public snackBar: MatSnackBar) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public openSnackBar(message: string, action: string) {
+        this.snackBar.open(message, action, {
+            duration: 2000,
+        });
+    }
 }
