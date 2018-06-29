492db81aacbd405eb2b96f38281195471859f9ce
a.gusejnov <gusejnov1999@list.ru>
2018-06-29 23:53:52 +0300

    update heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,12 +14,27 @@ export class HeroesComponent implements OnInit {
     constructor(private heroService: HeroService) {
     }
 
-    ngOnInit() {
+    public ngOnInit() {
         this.getHeroes();
     }
 
-    getHeroes(): void {
+    public delete(hero: Hero): void {
+        this.heroes = this.heroes.filter(h => h !== hero);
+        this.heroService.deleteHero(hero).subscribe();
+    }
+
+    public getHeroes(): void {
         this.heroService.getHeroes()
             .subscribe(heroes => this.heroes = heroes.data);
+    }
+
+    public add(name: string): void {
+        name = name.trim();
+        if (!name) { return; }
+        console.log(name)
+        this.heroService.addHero({ name } as Hero)
+            .subscribe(hero => {
+                this.heroes.push(hero.data);
+            });
     }
 }
